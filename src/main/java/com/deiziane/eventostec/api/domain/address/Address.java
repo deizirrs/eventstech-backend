3d26535c12fa65032d7bc81382459d3e48bcabbd
deizirrs <deizianesantos.rodrigues15@gmail.com>
2025-06-23 17:00:03 -0300

    feat: create Address entity

---
+++ src/main/java/com/deiziane/eventostec/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.deiziane.eventostec.api.domain.address;
+
+import java.util.UUID;
+
+import com.deiziane.eventostec.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "address")
+
+public class Address {
+	
+	@Id
+	@GeneratedValue	
+	private UUID id;
+	private String city;
+	private String uf;
+	
+		
+	@ManyToOne
+	@JoinColumn(name = "event_id")
+	private Event event;
+}
