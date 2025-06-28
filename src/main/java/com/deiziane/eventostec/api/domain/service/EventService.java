3fd34ae23ab2b5dbc32f20d6529992055c654f2c
deizirrs <deizianesantos.rodrigues15@gmail.com>
2025-06-28 02:04:03 -0300

    feat: implement event listing functionality with pagination

--- src/main/java/com/deiziane/eventostec/api/domain/service/EventService.java
+++ src/main/java/com/deiziane/eventostec/api/domain/service/EventService.java
@@ -4,17 +4,22 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.deiziane.eventostec.api.domain.event.Event;
 import com.deiziane.eventostec.api.domain.event.EventRequestDTO;
+import com.deiziane.eventostec.api.domain.event.EventResponseDTO;
 import com.deiziane.eventostec.api.domain.repositories.EventRepository;
 
 @Service
@@ -48,6 +53,22 @@ public class EventService {
 	  return savedEvent;
 	}
 
+	public List<EventResponseDTO> getEvents(int page, int size){
+		Pageable pageable = PageRequest.of(page, size);
+		Page<Event> eventsPage = this.eventRepository.findAll(pageable);
+		return eventsPage.map(event -> new EventResponseDTO(
+											event.getId(),
+											event.getTitle(),
+											event.getDescription(),
+											event.getDate(),
+											"",
+											"",
+											event.getRemote(),
+											event.getEventUrl(),
+											event.getImgUrl()))
+											.stream().toList();
+	}
+	
 	private String uploadImg(MultipartFile multipartFile) {
 
 		String fileName = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
