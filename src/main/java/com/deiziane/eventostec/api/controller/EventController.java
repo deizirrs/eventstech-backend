75ddaaf639d4eec055caa7ffa8658942b217044e
deizirrs <deizianesantos.rodrigues15@gmail.com>
2025-06-30 16:48:24 -0300

    feat: implement endpoint to retrieve event details by ID

--- src/main/java/com/deiziane/eventostec/api/controller/EventController.java
+++ src/main/java/com/deiziane/eventostec/api/controller/EventController.java
@@ -2,11 +2,13 @@ package com.deiziane.eventostec.api.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.deiziane.eventostec.api.domain.event.Event;
+import com.deiziane.eventostec.api.domain.event.EventDetailsDTO;
 import com.deiziane.eventostec.api.domain.event.EventRequestDTO;
 import com.deiziane.eventostec.api.domain.event.EventResponseDTO;
 import com.deiziane.eventostec.api.domain.service.EventService;
@@ -33,17 +36,20 @@ public class EventController {
 			@RequestParam(value = "image", required = false) MultipartFile image) {
 		
 		EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, uf, remote, eventUrl,image);
-		
 		Event newEvent = this.eventService.createEvent(eventRequestDTO);
-		
 		return ResponseEntity.ok(newEvent);
 
 	}
+	
+	@GetMapping("{eventId}")
+	public ResponseEntity<EventDetailsDTO> getEventDetails(@PathVariable UUID eventId){
+		EventDetailsDTO eventDetails = eventService.getEventsDetails(eventId);
+		return ResponseEntity.ok(eventDetails);
+		}
 
 	@GetMapping()
 	public ResponseEntity<List<EventResponseDTO>> getEvent(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
 	List<EventResponseDTO> allEvents = this.eventService.getUpComingEvents(page, size);
-
 	return ResponseEntity.ok(allEvents);
 	}
 
@@ -58,7 +64,6 @@ public class EventController {
 		
 	
 		  List<EventResponseDTO> events = eventService.getFilteredEvents(page, size, title, city, uf, startDate, endDate);
-	        
 		  return ResponseEntity.ok(events);
 	}
 }
